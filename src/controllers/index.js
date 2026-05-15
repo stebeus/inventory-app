6633a5900980b9995ebd48bbe9f8455d01444b76
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-15 14:44:28 -0300

    feat: query category data

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,19 @@
-const getIndex = (req, res) => res.render('index', { title: null });
+import {
+	selectAllCategories,
+	selectAllItems,
+	selectCategoryName,
+} from '#root/db/queries.js';
+
+const getIndex = async (req, res) => {
+	const {
+		params: { categoryId },
+	} = req;
+
+	const categories = await selectAllCategories();
+	const items = await selectAllItems(categoryId);
+	const title = await selectCategoryName(categoryId);
+
+	res.render('index', { title, categories, items });
+};
 
 export { getIndex };
