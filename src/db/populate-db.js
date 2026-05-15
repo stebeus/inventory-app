7246b2b555b18bc737b16ba527649260a9944927
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-15 18:22:46 -0300

    feat!: add items table

--- src/db/populate-db.js
+++ src/db/populate-db.js
@@ -3,18 +3,28 @@
 import { Client } from 'pg';
 
 import { DB_URL } from '#root/config.js';
-import { createTable } from '#root/utils/sql-queries.js';
+import { createTables, referenceForeignKey } from '#root/utils/sql-queries.js';
 
 const populateDb = async () => {
 	console.log('Seeding...');
 
 	const client = new Client({ connectionString: DB_URL });
 
-	const sql = createTable(
-		'categories',
-		'name VARCHAR (25) NOT NULL',
-		'img_url TEXT NOT NULL',
-		'timestamp TIMESTAMPTZ DEFAULT NOW()',
+	const sql = createTables(
+		[
+			'categories',
+			'name VARCHAR (25) NOT NULL',
+			'img_url TEXT NOT NULL',
+			'timestamp TIMESTAMPTZ DEFAULT NOW()',
+		],
+		[
+			'items',
+			referenceForeignKey('category', 'categories'),
+			'name VARCHAR (25) NOT NULL',
+			'description VARCHAR (250) NOT NULL',
+			'img_url TEXT NOT NULL',
+			'timestamp TIMESTAMPTZ DEFAULT NOW()',
+		],
 	);
 
 	await client.connect();
