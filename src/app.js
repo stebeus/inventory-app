72de1eff2f639134496945c3ad645ff76fed6553
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-15 14:58:21 -0300

    feat: route item

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import express from 'express';
 
 import { handleError, handleNotFoundError } from './controllers/error.js';
 import { index } from './routers/index.js';
+import { item } from './routers/item.js';
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(index);
+app.use(item);
 
 app.use(handleNotFoundError);
 app.use(handleError);
