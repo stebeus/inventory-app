ccf012db2918db74784613414e813a2086958838
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-15 16:03:05 -0300

    feat: add manage-categories router

---
+++ src/routers/manage-categories.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import { getManageCategories } from '#root/controllers/manage-categories.js';
+
+const router = Router();
+
+router.get('/manage-categories/:categoryId', getManageCategories);
+
+export { router as manageCategories };
