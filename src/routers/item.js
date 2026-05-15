7cdd2515c5408f6137dc8f41bff2a58cf489c2b2
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-15 14:58:09 -0300

    feat: add item router

---
+++ src/routers/item.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import { getItem } from '#root/controllers/item.js';
+
+const router = Router();
+
+router.get('/categories/:categoryId/item/:itemId', getItem);
+
+export { router as item };
